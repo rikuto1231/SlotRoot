23cb27e98f192e522fe12f7ecea4b2a7c14c694a
Shintate Rikuto <rikuriku.shin@gmail.com>
2024-11-08 10:12:58 +0900

    refactor：　戻るボタンの遷移ロジックを追加変更

--- Sample_game/Assets/Script/PlayerController.cs
+++ Sample_game/Assets/Script/PlayerController.cs
@@ -11,14 +11,13 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-        // Rigidbody2Dの取得
         rb = GetComponent<Rigidbody2D>();
         if (rb == null)
         {
             Debug.LogError("Rigidbody2D component is missing!");
         }
 
-        // カメラの左端と右端の座標を計算
+        // カメラの座標計算
         float cameraDistance = Camera.main.transform.position.z - transform.position.z;
         screenLeft = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, cameraDistance)).x;
         screenRight = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, cameraDistance)).x;
@@ -28,24 +27,22 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        // 左右の移動
         float moveInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
 
-        // キャラクターが右端を超えたら左端に移動
+        // 後ほどステージ入れかえのロジックを追加
         if (transform.position.x > screenRight)
         {
             transform.position = new Vector3(screenLeft, transform.position.y, transform.position.z);
             Debug.Log("Moved to left side of the screen");
         }
-        // キャラクターが左端を超えたら右端に移動
         else if (transform.position.x < screenLeft)
         {
             transform.position = new Vector3(screenRight, transform.position.y, transform.position.z);
             Debug.Log("Moved to right side of the screen");
         }
 
-        // ジャンプ処理
+        
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.velocity = Vector2.up * jumpForce;
@@ -54,7 +51,7 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // 地面に接触した時の処理
+
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
@@ -63,7 +60,6 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        // 地面から離れた時の処理
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = false;
